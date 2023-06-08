37de13dce11e46d3aa6fd6162336484bb9355a24
rriya1 <rriya1_be20@thapar.edu>
2023-06-09 01:30:19 +0530

    correct 25

--- TREES_take_you_forward/25_right_left_view_of_BT.cpp
+++ TREES_take_you_forward/25_right_left_view_of_BT.cpp
@@ -18,45 +18,99 @@ public:
 };
 
 
-
-void left_travel(Node* node, vector<int> &lt)
+vector<vector<int>> level_order(Node* root, vector<vector<int>>&v)
 {
-    if(node==NULL)
-        return;
-    lt.push_back(node->data);
-    if(node->left!=NULL)
-        left_travel(node->left,lt);
-    else
-        left_travel(node->right,lt);
-}
-
-void left_view(Node* root,vector<int> &left)
-{
+    //vector<int> level;
     if(root==NULL)
-        return;
-    left.push_back(root->data);
-    left_travel(root->left,left);
+        return v;
+    queue<Node*> node;
+    node.push(root);
+    while(!node.empty())
+    {
+        int size=node.size();
+        vector<int> level;
+        for(int i=0;i<size;i++)
+        {
+            Node* temp=node.front();
+            node.pop();
+            level.push_back(temp->data);
+            if(temp->left!=NULL)
+                node.push(temp->left);
+            if(temp->right!=NULL)
+                node.push(temp->right);
+        }
+        v.push_back(level);
+    }
+    return v;
 }
 
-
-void right_travel(Node* node, vector<int> &rt)
+void right_view(vector<vector<int>> v, vector<int> &r)
 {
-    if(node==NULL)
-        return;
-    rt.push_back(node->data);
-    if(node->right!=NULL)
-        right_travel(node->right,rt);
-    else
-        right_travel(node->left,rt);
+    int i;
+    for(auto val: v)
+    {
+        i=0;
+        int size=val.size();
+        for(auto it: val)
+        {
+            if(i==size-1)
+                r.push_back(it);
+            i++;
+        }
+    }
 }
 
-void right_view(Node* root,vector<int> &right)
+void left_view(vector<vector<int>> v, vector<int> &r)
 {
-    if(root==NULL)
-        return;
-    right.push_back(root->data);
-    right_travel(root->right,right);
+    int i;
+    for(auto val: v)
+    {
+        r.push_back(val[0]);
+    }
 }
+
+
+
+
+//idk i dont find recursice intutive here, and this code has some bugs
+// void left_travel(Node* node, vector<int> &lt)
+// {
+//     if(node==NULL)
+//         return;
+//     lt.push_back(node->data);
+//     if(node->left!=NULL)
+//         left_travel(node->left,lt);
+//     else
+//         left_travel(node->right,lt);
+// }
+
+// void left_view(Node* root,vector<int> &left)
+// {
+//     if(root==NULL)
+//         return;
+//     left.push_back(root->data);
+//     left_travel(root->left,left);
+// }
+
+
+// void right_travel(Node* node, vector<int> &rt)
+// {
+//     if(node==NULL)
+//         return;
+//     rt.push_back(node->data);
+//     if(node->right!=NULL)
+//         right_travel(node->right,rt);
+//     else
+//         right_travel(node->left,rt);
+// }
+
+// void right_view(Node* root,vector<int> &right)
+// {
+//     if(root==NULL)
+//         return;
+//     right.push_back(root->data);
+//     right_travel(root->right,right);
+// }
 
 
 
@@ -80,8 +134,10 @@ int main()
      5     6 */ 
   
   vector<int> rightv,leftv;
-  right_view(root,rightv);
-  left_view(root,leftv);
+  vector<vector<int>> v;
+   level_order(root,v);
+  right_view(v,rightv);
+  left_view(v,leftv);
 
   cout<<"right view: "<<endl;
   for( auto val:rightv)
