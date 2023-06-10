070f7db8130870740284870e07589c0aac4eecbe
rriya1 <rriya1_be20@thapar.edu>
2023-06-10 12:05:29 +0530

    revising recursion

---
+++ RECURSION_aditya_verma/3_print_1_to_n.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void print(int n)
+{
+    if(n==1)
+    {
+        cout<<1<<" ";
+        return;
+    }  
+    print(n-1);
+    cout<<n<<" ";
+    return;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
---
+++ RECURSION_aditya_verma/tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void print(int n)
+{
+    if(n==1)
+    {
+        cout<<1;
+        return;
+    }  
+    cout<<n<<" ";
+    print(n-1);
+    return;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
