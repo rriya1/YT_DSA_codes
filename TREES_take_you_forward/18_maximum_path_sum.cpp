4de2ce73e84d9808801046305d17f17a31a0bf1f
rriya1 <rriya1_be20@thapar.edu>
2023-06-13 22:27:01 +0530

    solved

--- TREES_take_you_forward/18_maximum_path_sum.cpp
+++ TREES_take_you_forward/18_maximum_path_sum.cpp
@@ -17,24 +17,51 @@ public:
 	}
 };
 
-int sum=0;
-int max_sum_path_help(Node* root)
+int sum=INT_MIN;
+
+int maxpathsum(Node* root)
 {
-    if(root==NULL)
-        return 0;
-    int lsum=max_sum_path_help(root->left);
-    int rsum=max_sum_path_help(root->right);
-    int s=max(lsum,rsum)+root->data;
-    sum=max(sum,s);
-    return s;
+	if(root==NULL)
+		return 0;
+	maxpath(root,sum);
+	return sum;
 }
 
-int maxx_sum_path(Node* node)
+int maxpath(Node* root, int &sum)
 {
-    int l=max_sum_path_help(root->left);
-    int r=max_sum_path_help(root->right);
-    
+	if(root==NULL)
+		return 0;
+	int left=max(0,maxpath(node->left,sum));
+	int right=max(0,maxpath(node->right,sum));
+	sum=max(sum,right+left+root->val);
+	return max(left,right)+root->val;
 }
+
+/*
+my code--------------->>>>>>>>>>>>>>>>>>>>mycode--------------------->>>>>>>>>>>>>my code
+int sum=INT_MIN;
+    int maxPathSum(TreeNode* root) 
+    {
+        if(root==NULL)
+            return 0;
+        int h=height(root);
+        return sum;
+    }
+
+    int height(TreeNode* root)
+    {
+        if(root==NULL)
+            return 0;
+        int lh=height(root->left);
+        int rh=height(root->right);
+        //if(!((lh>1 && rh==0) || (rh>1 && lh==0)))
+        int sum1=lh+rh+root->val;
+        sum=max(sum,sum1);
+        if(root->val<0)
+            return max(0,max(root->val+lh,rh+root->val));
+        //int sum1=lh+rh+root->val;
+        return max(root->val+lh,rh+root->val);
+    }*/
 
 
 int main()
@@ -55,8 +82,8 @@ int main()
          / \  
          5  6 */ 
     
-    int height = length_and_sum(root);
-    cout<<height<<endl;
+    int sum = maxpathsum(root);
+    //cout<<height<<endl;
     cout<<sum;//this is the answer
 	return 0;
 }
